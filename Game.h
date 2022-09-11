154fa4a3d92b5ebfd604115bc8e0272f601fcbb3
Nicolas <nicolas.paris@uha.fr>
2022-09-11 14:41:24 +0200

    Ascending diagonal & xy fix

--- Game.h
+++ Game.h
@@ -7,8 +7,8 @@
 
 #include <stdbool.h>
 
-void setToken(int (*board)[6][7], int i, int j, int v);
-void print_array2(const int (*)[6][7]);
-int victoryCheck(int (*)[6][7], int, int);
+void setToken(int (*board)[7][6], int i, int j, int v);
+void print_array2(const int (*)[7][6]);
+int victoryCheck(int (*)[7][6], int, int);
 
 #endif //PUISSANCEC_GAME_H
